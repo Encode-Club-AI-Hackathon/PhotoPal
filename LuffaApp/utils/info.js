9f1e96ee4a29550ba11169e0553724ff0caf34da
Arad Soutehkeshan <arad.skn84@gmail.com>
2026-03-21 22:21:16 +0000

    Add default info icon as a data URL in info.js

---
+++ LuffaApp/utils/info.js
@@ -0,0 +1,5 @@
+const defaultInfoIcon = 'data:image/svg+xml,%3Csvg%20xmlns=%22http://www.w3.org/2000/svg%22%20width=%2224%22%20height=%2224%22%20viewBox=%220%200%2024%2024%22%20fill=%22none%22%20stroke=%22currentColor%22%20stroke-width=%222%22%20stroke-linecap=%22round%22%20stroke-linejoin=%22round%22%20class=%22icon%20icon-tabler%20icons-tabler-outline%20icon-tabler-info-circle%22%3E%3Cpath%20stroke=%22none%22%20d=%22M0%200h24v24H0z%22%20fill=%22none%22/%3E%3Cpath%20d=%22M3%2012a9%209%200%201%200%2018%200a9%209%200%200%200%20-18%200%22%20/%3E%3Cpath%20d=%22M12%209h.01%22%20/%3E%3Cpath%20d=%22M11%2012h1v4h1%22%20/%3E%3C/svg%3E'
+
+module.exports = {
+    defaultInfoIcon
+}
