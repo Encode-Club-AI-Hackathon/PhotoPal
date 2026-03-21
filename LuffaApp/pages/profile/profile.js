cf89fe3ba82b3a3b923a49e7be26d57f39266eb0
Arad Soutehkeshan <arad.skn84@gmail.com>
2026-03-21 12:37:21 +0000

    Enhance profile page with user information display, session status, and permissions management

--- LuffaApp/pages/profile/profile.js
+++ LuffaApp/pages/profile/profile.js
@@ -1,5 +1,94 @@
+const app = getApp()
+
 Page({
   data: {
-    title: 'Profile'
+    title: 'Profile',
+    loadingProfile: false,
+    profileError: '',
+    userInfo: {},
+    hasUserInfo: false,
+    authSetting: {},
+    accountInfo: {},
+    sessionStatus: 'Unknown',
+    rawProfile: ''
+  },
+  onLoad: function () {
+    this.refreshAvailableInfo()
+  },
+  onShow: function () {
+    this.refreshAvailableInfo()
+  },
+  refreshAvailableInfo: function () {
+    const currentUser = app.globalData.userInfo || {}
+    this.setData({
+      userInfo: currentUser,
+      hasUserInfo: !!currentUser.nickName
+    })
+
+    wx.getSetting({
+      success: (res) => {
+        this.setData({
+          authSetting: res.authSetting || {}
+        })
+      }
+    })
+
+    try {
+      const account = wx.getAccountInfoSync()
+      this.setData({
+        accountInfo: (account && account.miniProgram) || {}
+      })
+    } catch (err) {
+      this.setData({
+        accountInfo: {}
+      })
+    }
+
+    wx.checkSession({
+      success: () => {
+        this.setData({ sessionStatus: 'Valid' })
+      },
+      fail: () => {
+        this.setData({ sessionStatus: 'Expired' })
+      }
+    })
+  },
+  getUserProfile: function () {
+    this.setData({
+      loadingProfile: true,
+      profileError: ''
+    })
+
+    wx.getUserProfile({
+      desc: 'Used to display your profile details in PhotoPal.',
+      lang: 'en',
+      success: (res) => {
+        const info = res.userInfo || {}
+        app.globalData.userInfo = info
+
+        this.setData({
+          userInfo: info,
+          hasUserInfo: !!info.nickName,
+          rawProfile: JSON.stringify(res, null, 2)
+        })
+      },
+      fail: (err) => {
+        this.setData({
+          profileError: (err && err.errMsg) || 'Failed to fetch profile information.'
+        })
+      },
+      complete: () => {
+        this.setData({
+          loadingProfile: false
+        })
+      }
+    })
+  },
+  openSettings: function () {
+    wx.openSetting({
+      success: () => {
+        this.refreshAvailableInfo()
+      }
+    })
   }
 })
